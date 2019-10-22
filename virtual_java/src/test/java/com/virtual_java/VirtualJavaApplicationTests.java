98f36cf97caba1dbe00964b23f0cd5656bc0add9
xiaohan0104 <51428239+xiaohan0104@users.noreply.github.com>
2019-10-22 15:57:03 +0800

    fix

---
+++ virtual_java/src/test/java/com/virtual_java/VirtualJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtual_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

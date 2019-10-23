8e417145723433ad3f930d7c317ee7587cd789cf
sangshuo <787184827@qq.com>
2019-10-23 12:49:41 +0800

    fix:服务器配置及数据库，宝塔地址.xls  sangshuo

--- virtual/src/components/login/Login.vue
+++ virtual/src/components/login/Login.vue
@@ -1,10 +1,20 @@
 <template>
-    <el-container></el-container>
+    <el-container>
+        <div>
+            账号：<el-input v-model="input" placeholder="请输入内容"></el-input>
+            密码：<el-input v-model="input" type="password" placeholder="请输入内容"></el-input>
+        </div>
+    </el-container>
 </template>
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data() {
+    return {
+      input: ''
+    };
+  }
 };
 </script>
 

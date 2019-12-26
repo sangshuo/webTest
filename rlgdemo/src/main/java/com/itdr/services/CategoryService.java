3d8f8db8d79164aad0fe440626b1ea6df8f5f8dc
xiaohan0104 <51428239+xiaohan0104@users.noreply.github.com>
2019-12-26 15:09:34 +0800

    fix:测试

--- rlgdemo/src/main/java/com/itdr/services/CategoryService.java
+++
@@ -1,8 +0,0 @@
-package com.itdr.services;
-
-import com.itdr.common.ServiceResponse;
-
-public interface CategoryService {
-    //根据分类ID查询所有子类
-    ServiceResponse getDeepCategory(Integer categoryId);
-}
--- rlgdemo/src/main/java/com/itdr/services/ProductService.java
+++
@@ -1,8 +0,0 @@
-package com.itdr.services;
-
-import com.itdr.common.ServiceResponse;
-import com.itdr.pojo.Product;
-
-public interface ProductService {
-    ServiceResponse getProductOneById(Integer pid);
-}
